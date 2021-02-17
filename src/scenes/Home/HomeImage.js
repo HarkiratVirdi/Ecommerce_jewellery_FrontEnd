08981014e7f3a5a79d6187804d55a263948db4a0
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-02-17 14:26:43 -0500

    fixed logout bug

--- src/scenes/Home/HomeImage.js
+++ src/scenes/Home/HomeImage.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ImageFull from "../../images/fullPageEdited.jpg";
 import ImageHalf from "../../images/joeyy_lee_7tdqxrc8hh_TjQPx.jpg";
+import gsap from "gsap";
 
 const HomeImage = () => {
+  let Image2 = useRef(null);
+  let homeImage = useRef(null);
+  useEffect(() => {
+    gsap.to(
+      Image2,
+      {
+        scrollTrigger: {
+          trigger: Image2,
+          start: "top center",
+          toggleActions: "restart pause reverse pause",
+          scrub: true,
+        },
+        y: -20,
+      },
+      []
+    );
+
+    gsap.to(
+      homeImage,
+      {
+        scrollTrigger: {
+          trigger: homeImage,
+          start: "top center",
+          toggleActions: "restart pause reverse pause",
+          scrub: true,
+        },
+        transform: "matrix(1.15,0,0,1.15,0, 0)",
+      },
+      []
+    );
+  });
+
   return (
-    <section className="Home_image">
+    <section
+      className="Home_image"
+      ref={(el) => {
+        homeImage = el;
+      }}
+    >
       <div className="Home_image_img">
-        <img src={ImageHalf} alt="" />
+        <img
+          ref={(el) => {
+            Image2 = el;
+          }}
+          src={ImageHalf}
+          alt=""
+        />
       </div>
       <div className="Home_image_content">
         <div className="Home_image_content_heading heading-1 heading-1--white">
